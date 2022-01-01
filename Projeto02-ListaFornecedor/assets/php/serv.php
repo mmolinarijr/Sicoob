c7c4baff237db861be0d45efa0e7eecb73328b71
Mauricio Molinari <mmolinarijr@gmail.com>
2022-01-01 16:01:40 -0300

    cadastro

--- Projeto02-ListaFornecedor/assets/php/serv.php
+++ Projeto02-ListaFornecedor/assets/php/serv.php
@@ -1,6 +1,6 @@
 <?php 
-include("assets/php//conn.php"); 
-$sql = "INSERT INTO cadastro VALUES (null, '$_POST[nome]', '$_POST[telefone]')"; 
+include("assets/php/conn.php"); 
+$sql = "INSERT INTO Cadastro VALUES (null, '$_POST[empresa]', '$_POST[nome]', '$_POST[cpfcnpj]', '$_POST[telefone]', ;$_POST[datahora]')"; 
 $query = mysql_query($sql); 
 if ($query) header("Location: listforn.php"); 
 else echo "Erro ao inserir registro. Provavelmente registro já cadastrado"; 
